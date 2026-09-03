d6455f8a7b572f5dbcacd352e6deed81416d401e
Aysha Anoofa <aysha.anoofa@segnitos.in>
2026-09-04 02:21:34 +0530

    Configure Vercel API deployment

--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
 app.use(async (req, res, next) => {
   try {
     await connectDB();
